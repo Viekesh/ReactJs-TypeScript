02a46069f25022ca4ddced1c20bc9bf93aa57879
Viekesh <vikeshgaikwad1@gmail.com>
2022-10-15 10:21:14 +0100

    Continue To Code Food Delivry App 1

--- src/Projects/FoodDeliveryApp1/Pages/HomePage.jsx
+++ src/Projects/FoodDeliveryApp1/Pages/HomePage.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import { useState } from "react";
+import Footer from "../Components/Common/Footer";
+import Header from "../Components/Common/Header";
+import TabOptions from "../Components/Common/TabOptions";
+import "../Styles/HomePage.css";
+
+
 
 const HomePage = () => {
+
+  // "TabOptions" have three different screens
+  // we are use here "useState" hook, state will decide which screen we are seen crrently
+  // "useState" hook provides a function and variable, we can able to change already stored value
+  // in that variable
+  // syntax :
+  // const randomName [variable, function] = useState("initial value")
+
+  // according to your current need you can paas value in "useState", sometimes you can pass
+  // blank space because many cases there is no value we can pass in it
+  // here we will use our initial value is "Delivery", it is one of the tab options screen
+  // which we want to see on our screen, so our default screen is delivery.
+
+  const [activeTab, setActiveTab] = useState("Delivery");
+
   return (
-    <div>This is homepage</div>
+
+    <div className="home_page">
+
+      <Header />
+
+      <TabOptions />
+      {getCorrectScreen(activeTab)}
+
+      <Footer />
+
+    </div>
   )
+}
+
+const getCorrectScreen = (tab) => {
+  switch (tab) {
+    case "Delivery":
+      return <div>Delivery</div>
+
+    case "Dining Out":
+      return <div>Dining Out</div>
+    
+    case "Nightlife" :
+      return <div>Nightlife</div>
+
+    default:
+      return <div>Delivery</div>
+  }
 }
 
 export default HomePage;
