ca62b3dd4498fccb8279dde84cc0e9ce0553f380
Viekesh <vikeshgaikwad1@gmail.com>
2022-11-07 18:05:03 +0000

    Practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./App.css";
 import ProjectRoutes from "./Projects/MainComponents/ProjectRoutes";
-import {app} from "./firebaseConfig";
 
 
 
--- src/Projects/MainComponents/LandingPage/Components/LandingPage.tsx
+++ src/Projects/MainComponents/LandingPage/Components/LandingPage.tsx
@@ -1,12 +1,16 @@
 import Navigation from './Navigation';
 import "../Styles/LandingPage.css";
 import PortfolioLinks from './PortfolioLinks';
+// import Tuts from '../../../Tuts/Tuts';
+import StudentInfo from '../../../Tuts/Tuts';
 
 
 const LandingPage = () => {
   return (
     <div className='landing_page'>
         <Navigation />
+        {/* <Tuts /> */}
+        <StudentInfo />
         <PortfolioLinks />
     </div>
   )
