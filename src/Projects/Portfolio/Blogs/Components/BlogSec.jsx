fe37c4d08f12205874568aacbc7b7be16a3739a5
Viekesh <vikeshgaikwad1@gmail.com>
2023-07-09 21:04:25 +0530

    Continue To work with Blogs And BlogSec Component

--- src/Projects/Portfolio/Blogs/Components/BlogSec.jsx
+++ src/Projects/Portfolio/Blogs/Components/BlogSec.jsx
@@ -3,51 +3,53 @@ import "./BlogSec.scss";
 
 
 
-const BlogSec = ({blogs}) => {
-  return (
-    <div>
-        <div className='blog_sec'>
-            {
-                blogs?.map((item) => {
-                    <div className="box_1" key={item.id}>
-                        <div className="blog_container">
-                            <div className="blog_img">
-                                <img src={item.imgUrl} alt={item.title} />
+const BlogSec = ({ blogs }) => {
+    return (
+        <div>
+            <div className='blog_sec'>
+                {
+                    blogs?.map((item) => {
+                        return (
+                            <div className="box_1" key={item.id}>
+                                <div className="blog_container">
+                                    <div className="blog_img">
+                                        <img src={item.imgUrl} alt={item.title} />
+                                    </div>
+                                </div>
+
+                                <div className="text_area">
+
+                                    <div className="text_start_from_here">
+                                        <h6 className='categories'>{item.category}</h6>
+                                        {/* <span>{item.title}</span> */}
+                                        <span className='meta_info'>
+                                            <p>{item.author}</p>
+                                            {/* {item.timestamp.toDate()} */}
+                                        </span>
+                                    </div>
+
+                                    <div className="short_description">
+
+                                        {/* It can receive the string along with the count like up to how much letter you want to display. */}
+                                        {exerpt(item.description, 120)}
+
+                                    </div>
+
+                                    <button className="read_more_btn">read more</button>
+
+                                    <div className="update_delete">
+                                        <button className="update_btn">update</button>
+                                        <button className="delete_btn">delete</button>
+                                    </div>
+
+                                </div>
                             </div>
-                        </div>
-
-                        <div className="text_area">
-
-                            <div className="text_start_from_here">
-                                <h6 className='categories'>{item.category}</h6>
-                                <span>{item.title}</span>
-                                <span className='meta_info'>
-                                    <p>{item.author}</p>
-                                    {item.timestamp.toDate().toDateString()}
-                                </span>
-                            </div>
-
-                            <div className="short_description">
-
-                                {/* It can receive the string along with the count like up to how much letter you want to display. */}
-                                {exerpt(item.description, 120)}
-
-                            </div>
-
-                            <button className="read_more_btn">read more</button>
-
-                            <div className="update_delete">
-                                <button className="update_btn">update</button>
-                                <button className="delete_btn">delete</button>
-                            </div>
-
-                        </div>
-                    </div>
-                })
-            }
+                        )
+                    })
+                }
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default BlogSec;
--- src/Projects/Portfolio/Blogs/Pages/Blogs.jsx
+++ src/Projects/Portfolio/Blogs/Pages/Blogs.jsx
@@ -3,27 +3,59 @@ import BottomNav from '../../Navigation/BottomNav';
 import PageHeading from '../../../Components/PageHeading/PageHeading';
 import BlogHeader from '../Components/BlogHeader';
 import { useEffect, useState } from 'react';
-import { auth } from '../../../../FirebaseConfig';
+import { auth, database } from '../../../../FirebaseConfig';
 import BlogSec from '../Components/BlogSec';
+import { collection, onSnapshot } from 'firebase/firestore';
+
+
 
 const Blogs = () => {
+
+    const [loading, setLoading] = useState(true);
+
+    const [blog, setBlog] = useState([]);
 
     // We have to persist loggedIn user in our application and we need some user information as well
     // like display name, So here we can define useState (const [user, setUser] = useState(null);)
     // Initially it will be null
-    const[user, setUser] = useState(null);
+    const [user, setUser] = useState(null);
+
+    const unsub = onSnapshot(
+        collection(database, "BlogApp1Data"),
+        (snapshot) => {
+            const list = [];
+            snapshot.docs.forEach((doc) => {
+                list.push({
+                    id: doc.id,
+                    ...doc.data(),
+                });
+            });
+
+            setBlog(list);
+        },
+        (error) => {
+            console.log(error.message);
+            alert(error.message);
+        },
+    );
 
     useEffect(() => {
 
         auth.onAuthStateChanged((authUser) => {
-            if(authUser) {
+            if (authUser) {
                 setUser(authUser);
             } else {
                 setUser(null);
             }
         });
 
-    }, []);
+        return () => {
+            unsub();
+        };
+
+    }, [unsub]);
+
+    console.log("BlogApp1Data : ", blog);
 
     return (
         <div>
@@ -37,7 +69,7 @@ const Blogs = () => {
                     <div className="row_container">
                         <h2>trending</h2>
                         <section className="blog_section">
-                            <BlogSec />
+                            <BlogSec blogs={blog} />
                         </section>
                         <section className="tag_section">
                             <h2>tags</h2>
